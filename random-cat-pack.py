f6bf77c3bbeb0dfb27cbf6e52b84a1e2154ada2a
Katie Quesnel <katie_quesnel@yahoo.ca>
2025-03-06 00:00:48 -0400

    formated a file

--- random-cat-pack.py
+++ random-cat-pack.py
@@ -4,6 +4,7 @@ import random
 from describecat import describe
 from cat_creation_methods import get_prefix, get_suffix, random_genestring
 
+
 def create_cat_entry():
     genes = random_genestring()
     appearance = describe(genes)
@@ -12,11 +13,12 @@ def create_cat_entry():
     suffix = get_suffix(prefix, age)
     print(f"{prefix}{suffix}, {appearance}. {age} moons old")
 
+
 def main():
     clansize = input("How many cats would you like to generate?")
     for _ in range(int(clansize)):
         create_cat_entry()
-    
+
 
 if __name__ == "__main__":
     main()
